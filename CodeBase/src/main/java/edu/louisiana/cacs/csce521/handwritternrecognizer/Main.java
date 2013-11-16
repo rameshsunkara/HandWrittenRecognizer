c1eef5a7d5748cd6480d08ec1bfb0a2c2ffb1d7b
Ramesh Sunkara <jvmrider@gmail.com>
2013-11-16 16:44:52 -0600

    Initial commit. Created skeleton and added description document.

---
+++ CodeBase/src/main/java/edu/louisiana/cacs/csce521/handwritternrecognizer/Main.java
@@ -0,0 +1,30 @@
+package edu.louisiana.cacs.csce521.handwritternrecognizer;
+
+import java.io.File;
+
+import edu.louisiana.cacs.csce521.handwritternrecognizer.util.Configurator;
+
+/**
+ * <p>Main class which calls required constructors like 
+ *      
+ *  </p>
+ *  <p>It also configures the system by reading the HandWrittenRecognizer.properties.<p>
+ * @author rsunkara
+ * @since November 16,2013
+ *
+ */
+public class Main {
+
+	public static void main(String []args){
+		
+		Configurator xConfigurator = new Configurator();
+		String xPropertiesFilePath = System.getProperty("user.dir") + File.separator
+				+ "src" + File.separator + "main" + File.separator
+				+ "resources" + File.separator + "HandWrittenRecognizer.properties";
+		//If any error in system configuration, stop the system
+		if(!xConfigurator.loadConfigValues(xPropertiesFilePath))
+			return;
+		
+	}
+
+}
---
+++ CodeBase/src/main/java/edu/louisiana/cacs/csce521/handwritternrecognizer/exception/IRException.java
@@ -0,0 +1,20 @@
+package edu.louisiana.cacs.csce521.handwritternrecognizer.exception;
+
+/**
+ * 
+ * Custom exception class for Information Retrieval operations
+ * @author rsunkara
+ *
+ */
+public class IRException extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public IRException(String message) {
+        super(message);
+    }
+
+}
